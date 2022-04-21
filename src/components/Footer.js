327d9582da5c8e948d94a9769697ffd30af3a3fc
Saad Elbeleidy <sbeleidy90@gmail.com>
2022-04-21 17:13:41 -0400

    make name in footer linkable

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,10 @@ const Footer = class extends React.Component {
   render() {
     return (
       <footer className="bg-black text-white text-center p-2">
-        Built with ❤️ by Saad Elbeleidy
+        Built with ❤️ by{" "}
+        <a className="text-white hover:text-gray-300" href="/">
+          Saad Elbeleidy
+        </a>
       </footer>
     );
   }
